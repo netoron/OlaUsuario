4e1ddf5cee72c636f684ccf84dc5f8ee7a4b12a0
netoron <61334943+netoron@users.noreply.github.com>
2020-05-15 23:53:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digitite seu Nome");
+            String nome = Console.ReadLine();
+            Console.Write("Olá!");
+            Console.WriteLine(nome);
+        }
+    }
+}
